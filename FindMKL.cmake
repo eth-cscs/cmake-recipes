3e3ae5efcf9c1a0d37135ea1ceb1e91c88ebd63e
Teodor Nikolov <teodor.nikolov22@gmail.com>
2019-10-04 15:51:16 +0200

    Merge FindMKL and FindMKL_ScaLAPACK
    - Implement ScaLAPACK as a component of FindMKL.
    - Improve FindMKL documentation
    - Move function declarations to the top
    - Use CACHE variables for options but guard them with `NOT DEFINED`
    - Rename OPENMPI variables to be project agnostic

--- FindMKL.cmake
+++ FindMKL.cmake
@@ -1,38 +1,113 @@
-# Uses MKLROOT environment variable or CMake's MKL_ROOT to find MKL.
-#
-# Imported Targets: 
-#   MKL::MKL
-#
-# The type of threading for MKL has to be specified using the variable
-#        MKL_PARALLEL            := ON|OFF   (default: ON / parallel)
-#        MKL_64BIT               := ON|OFF   (default: OFF / 32bit interface)
-#
-# NOT SUPPORTED
-#   - TBB threading back-end
-#   - F95 interfaces
-#
-# Note: Mixing GCC and Intel OpenMP backends is a bad idea.
-#       The module depends on FindThreads and FindOpenMP.
+#[=======================================================================[.rst:
+FindMKL
+-------
+
+Example usage
+^^^^^^^^^^^^^
+
+  find_package(MKL REQUIRED OPTIONAL_COMPONENTS SCALAPACK)
+  target_link_libraries(... MKL::MKL)
+  target_link_libraries(... MKL::SCALAPACK)
+
+Note: Targets linking to MKL::SCALAPACK need not link to MKL::MKL. It is NOT 
+      an error if both targets are linked though.
+
+
+Options
+^^^^^^^
+
+``MKLROOT``
+  Environment variable set to MKL's root directory  
+
+``MKL_ROOT``
+  CMake variable set to MKL's root directory  
+
+``MKL_PARALLEL``           
+  ON|OFF   (default: ON / parallel)
+
+``MKL_64BIT``
+  ON|OFF   (default: OFF / 32bit interface)
+
+
+Components
+^^^^^^^^^^
+
+``SCALAPACK``
+
+
+Imported targets 
+^^^^^^^^^^^^^^^^
+
+``MKL::MKL``
+
+``MKL::SCALAPACK``
+
+
+Result variables
+^^^^^^^^^^^^^^^^
+
+``MKL_FOUND``
+  Found MKL.
+
+``MKL_SCALAPACK_FOUND``
+  Found ScaLAPACK.
+
+Not supported
+^^^^^^^^^^^^^
+
+- TBB threading back-end
+- F95 interfaces
+
+Note: Mixing GCC and Intel OpenMP backends is a bad idea.
+
+#]=======================================================================]
+
+# Modules
 #
 include(FindPackageHandleStandardArgs)
 
+# Functions
+#
+function(__mkl_find_library _name)
+    find_library(${_name}
+        NAMES ${ARGN}
+        HINTS ${MKL_ROOT}
+        PATH_SUFFIXES ${_mkl_libpath_suffix}
+                      lib
+        )
+    mark_as_advanced(${_name})
+endfunction()
+
+# Options
+#
+# The `NOT DEFINED` guards on CACHED variables are needed to make sure that 
+# normal variables with of same name always take precedence*.
+#
+# * There are many caveats with CACHE variables in CMake. Before version 
+#   3.12, both the `option()` command and `set(... CACHE ...)` would 
+#   override normal variables if cached equivalents don't exist or they 
+#   exisit but their type is not specified (e.g. command line arguments: 
+#   -DFOO=ON instead of -DFOO:BOOL=ON). For 3.13 with policy CMP0077, 
+#   `option()` no longer overrides normal variables of the same name.
+#   `set(... CACHE ...)` is still stuck with the old behaviour. 
+#
+#   https://cmake.org/cmake/help/v3.15/command/set.html#set-cache-entry
+#   https://cmake.org/cmake/help/v3.15/command/set.html#set-cache-entry
+#
 if(NOT DEFINED MKL_ROOT)
-    set(MKL_ROOT $ENV{MKLROOT})
+    set(MKL_ROOT $ENV{MKLROOT} CACHE PATH "MKL's root directory.")
 endif()
 
 if(NOT DEFINED MKL_PARALLEL)
-    set(MKL_PARALLEL ON)
+    set(MKL_PARALLEL ON CACHE BOOL "Toggles parallel and sequential versions.")
 endif()
 
 if(NOT DEFINED MKL_64BIT)
-    set(MKL_64BIT OFF)
+    set(MKL_64BIT OFF CACHE BOOL "Toggles 32bit/64bit MKL interfaces.")
 endif()
 
-find_path(MKL_INCLUDE_DIR mkl.h
-    HINTS ${MKL_ROOT}/include
-    )
-mark_as_advanced(MKL_INCLUDE_DIR)
-
+# Determine MKL's library folder
+#
 set(_mkl_libpath_suffix "lib/intel64")
 if(CMAKE_SIZEOF_VOID_P EQUAL 4) # 32 bit
     set(_mkl_libpath_suffix "lib/ia32")
@@ -46,44 +121,74 @@ else ()
     string(APPEND _mkl_libpath_suffix "_lin")
 endif ()
 
-function(__mkl_find_library _name)
-    find_library(${_name}
-        NAMES ${ARGN}
-        HINTS ${MKL_ROOT}
-        PATH_SUFFIXES ${_mkl_libpath_suffix}
-                      lib
-        )
-    mark_as_advanced(${_name})
-endfunction()
-
-
-__mkl_find_library(MKL_CORE_LIB mkl_core)
-
+# Determine 32bit or 64bit interface is used
+#
 set(_mkl_lp "lp64")
-mark_as_advanced(${_mkl_lp})
 if(MKL_64BIT)
-  set(_mkl_lp "ilp64")
+    set(_mkl_lp "ilp64")
 endif()
+
+# Find MKL header
+#
+find_path(MKL_INCLUDE_DIR mkl.h
+    HINTS ${MKL_ROOT}/include
+    )
+mark_as_advanced(MKL_INCLUDE_DIR)
+
+# Find MKL core libraries
+#
+__mkl_find_library(MKL_CORE_LIB mkl_core)
 __mkl_find_library(MKL_INTERFACE_LIB mkl_intel_${_mkl_lp})
 
-if(MKL_PARALLEL)
-    if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" AND NOT APPLE)
+if (MKL_PARALLEL)
+    if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU" AND NOT APPLE)
         __mkl_find_library(MKL_THREADING_LIB mkl_gnu_thread)
-    else()
+    else ()
         __mkl_find_library(MKL_THREADING_LIB mkl_intel_thread)
-    endif()
-else()
+    endif ()
+else ()
     __mkl_find_library(MKL_THREADING_LIB mkl_sequential)
+endif ()
+
+# Find ScaLAPACK
+#
+find_package(MPI)
+if (MPI_FOUND)
+    __mkl_find_library(MKL_SCALAPACK_LIB mkl_scalapack_${_mkl_lp})
+
+    # Note: this requires running `mpirun`.
+    #
+    if (NOT OPENMPI_FOUND)
+        include(${CMAKE_CURRENT_LIST_DIR}/check_for_openmpi.cmake)
+        check_for_openmpi()
+    endif()
+    
+    if(OPENMPI_FOUND) # OpenMPI
+        if(APPLE)
+            message(FATAL_ERROR "Only MPICH is supported on Apple.")
+        endif()
+         __mkl_find_library(MKL_BLACS_LIB mkl_blacs_openmpi_${_mkl_lp})
+    else()            # MPICH
+        if(APPLE)
+            __mkl_find_library(MKL_BLACS_LIB mkl_blacs_mpich_${_mkl_lp})
+        else()
+            __mkl_find_library(MKL_BLACS_LIB mkl_blacs_intelmpi_${_mkl_lp})
+        endif()
+    endif()
+
+    find_package_handle_standard_args(MKL_SCALAPACK REQUIRED_VARS MKL_BLACS_LIB
+                                                                  MKL_SCALAPACK_LIB)
 endif()
 
+find_package_handle_standard_args(MKL REQUIRED_VARS MKL_CORE_LIB 
+                                                    MKL_THREADING_LIB 
+                                                    MKL_INTERFACE_LIB 
+                                                    MKL_INCLUDE_DIR
+                                      HANDLE_COMPONENTS)
 
-find_package_handle_standard_args(MKL 
-    DEFAULT_MSG  MKL_CORE_LIB
-    MKL_THREADING_LIB
-    MKL_INTERFACE_LIB
-    MKL_INCLUDE_DIR
-    )
 
+# Define the MKL::MKL targets
+#
 if (MKL_FOUND AND NOT TARGET MKL::MKL)
     find_package(Threads REQUIRED)
 
@@ -106,5 +211,17 @@ if (MKL_FOUND AND NOT TARGET MKL::MKL)
     set_target_properties(MKL::MKL PROPERTIES 
         INTERFACE_INCLUDE_DIRECTORIES "${MKL_INCLUDE_DIR}"
         INTERFACE_LINK_LIBRARIES "MKL::BLAS_INTERFACE;MKL::THREADING;MKL::CORE;${_mkl_threading_backend};Threads::Threads")
-endif()
 
+    # Define the MKL::SCALAPACK targets
+    #
+    if (MKL_SCALAPACK_FOUND AND NOT TARGET MKL::SCALAPACK)
+        add_library(MKL::BLACS UNKNOWN IMPORTED)
+        set_target_properties(MKL::BLACS PROPERTIES IMPORTED_LOCATION ${MKL_BLACS_LIB})
+
+        add_library(MKL::SCALAPACK UNKNOWN IMPORTED)
+        set_target_properties(MKL::SCALAPACK PROPERTIES 
+          IMPORTED_LOCATION "${MKL_SCALAPACK_LIB}"
+          INTERFACE_LINK_LIBRARIES "MKL::BLAS_INTERFACE;MKL::THREADING;MKL::CORE;MKL::BLACS;${_mkl_threading_backend};Threads::Threads"
+          )
+    endif()
+endif()
--- check_for_openmpi.cmake
+++ check_for_openmpi.cmake
@@ -1,11 +1,11 @@
-# Sets COSMA_WITH_OPENMPI to ON if OpenMPI was found and to OFF otherwise.
+# Sets OPENMPI_FOUND to ON if OpenMPI was found and to OFF otherwise.
 #
 function(check_for_openmpi)
   execute_process(COMMAND mpirun --version OUTPUT_VARIABLE MPIRUN_OUTPUT)
   string(FIND "${MPIRUN_OUTPUT}" "Open MPI" OMPI_POS)
   if(OMPI_POS STREQUAL "-1")
-    set(COSMA_WITH_OPENMPI OFF PARENT_SCOPE)
+    set(OPENMPI_FOUND OFF PARENT_SCOPE)
   else()
-    set(COSMA_WITH_OPENMPI ON PARENT_SCOPE)
+    set(OPENMPI_FOUND ON PARENT_SCOPE)
   endif()
 endfunction()
